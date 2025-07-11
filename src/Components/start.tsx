107d7446916d67447a52a4e6ae441dae2f0d06ee
Ethan9792 <ethanmichael@live.com>
2025-07-11 18:24:46 +0100

    2nd Commit, test

---
+++ src/Components/start.tsx
@@ -0,0 +1,10 @@
+function start() {
+  return (
+    <div className="p-4 border rounded shadow">
+      <h2>My Start is here!</h2>
+      <p>This is my first component.</p>
+    </div>
+  );
+}
+
+export default Hello;
